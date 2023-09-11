83b910c60a4c97bdc2eeabe18d3e65c44f391103
Myles Moylan <myles.moylan@gmail.com>
2023-09-11 16:01:02 -0700

    experiment with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
 import * as React from 'react';
 
-const welcome = {
-  greeting: 'Hello',
-  title: 'React',
-};
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    numComments: 3,
+    points: 4,
+    objectId: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    numComments: 2,
+    points: 5,
+    objectId: 1
+  },
+]
 
 function App() {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
+      <hr />
+      <ul>
+        {list.map(function (item) {
+          return <li key={item.objectId}>
+                    <a href={item.url}>{item.title}</a>
+                    <ul>
+                      <li>Author(s): {item.author}</li>
+                      <li>Comments: {item.numComments}</li>
+                      <li>Points: {item.points}</li>
+                    </ul>
+                 </li>;
+        })}
+      </ul>
     </div>
   );
 }
