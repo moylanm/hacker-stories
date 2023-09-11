71fdfb4531559d1ac12e4897dbeaf470c5ad41e6
Myles Moylan <myles.moylan@gmail.com>
2023-09-11 15:11:06 -0700

    add label, input, and greeting variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 
+const welcome = {
+  greeting: 'Hello',
+  title: 'React',
+};
+
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
     </div>
   );
 }
