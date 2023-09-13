dee544cdbd0ee2c0159d0e1869e61d2dedaceb67
Myles Moylan <moylanm@fastmail.com>
2023-09-13 09:22:22 -0700

    use react fragments intead of div elements

--- src/App.jsx
+++ src/App.jsx
@@ -57,10 +57,10 @@ const App = () => {
 }
 
 const Search = ({ searchTerm, onSearch }) => (
-  <div>
+  <>
     <label htmlFor="search">Search: </label>
     <input id="search" type="text" value={searchTerm} onChange={onSearch} />
-  </div>
+  </>
 );
 
 Search.propTypes = {
