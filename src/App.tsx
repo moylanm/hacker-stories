ed028d2114249e1f57977a5355cf4bbc34865ce4
Myles Moylan <moylanm@fastmail.com>
2023-09-14 08:01:37 -0700

    use memoized function for search term handling

--- src/App.tsx
+++ src/App.tsx
@@ -106,7 +106,7 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   );
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
 
     dispatchStories({ type: STORIES_FETCH_INIT });
@@ -121,6 +121,10 @@ const App = () => {
       })
       .catch(() => dispatchStories({ type: STORIES_FETCH_FAILURE }));
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item: Story) => {
     dispatchStories({
